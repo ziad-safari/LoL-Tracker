75a94cd86cb66eba6c28d3283460887b32854262
Vince Li <chickentaco823@gmail.com>
2022-02-20 18:25:50 -0500

    fixing summonersDAO

--- backend/api/dao/summonersDAO.js
+++ backend/api/dao/summonersDAO.js
@@ -1,1 +1,17 @@
 let summoners
+
+export default class summonersDAO {
+    static async injectDB(conn) {
+        if (summoners) {
+            return
+        }
+        try { 
+            summoners = await conn.db(process.env.TRACKER_NS).collection("summoners")
+        } catch (e) {
+            console.error(
+                'Unable to establish a collection handle in restaurantsDAO: ' +e,
+            )
+        }
+    }
+}
+
